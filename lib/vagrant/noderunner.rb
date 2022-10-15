fa1cde6e64c42869d96dfa9898e976d778e2f2ca
Thomas Åkesson <thomas.akesson@simonsoft.se>
2022-10-15 08:00:14 +0200

    Some providers, like 'aws', require reload in order to sync updated cookbooks before provision.

--- lib/vagrant/noderunner.rb
+++ lib/vagrant/noderunner.rb
@@ -255,7 +255,10 @@ def main(node: "labs01", provider: "virtualbox", retest: false, guestint: true, 
       exit 0
     else
       puts "Vagrant node running, provision..."
-      vagrant_cmd = cwd_to_node + "vagrant provision"
+      # Some providers, like 'aws', require reload in order to sync updated cookbooks before provision.
+      # Execute individual tests instead when quick iterations are needed.
+      # TODO: consider condition based on #{provider}.
+      vagrant_cmd = cwd_to_node + "vagrant reload --provision"
     end
   else
     $stderr.puts "Unknown Vagrant state: #{v_status}"
